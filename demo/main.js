df99df872a60cf75b6d338103c215024bbc80196
Seungjae Park <astro.psj@gmail.com>
2019-01-04 01:58:52 +0900

    Add docs

---
+++ demo/main.js
@@ -0,0 +1,18 @@
+import('../pkg/particles')
+  .then(({ ParticleManager }) => {
+    const canvas = document.getElementById('particles');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    const context = canvas.getContext("2d");
+    context.fillStyle = 'white';
+    context.strokeStyle = 'white';
+
+    const particleManager = new ParticleManager(canvas, 100);
+    const frameRequetCallback = () => {
+      particleManager.update();
+      window.requestAnimationFrame(frameRequetCallback);
+    }
+    window.requestAnimationFrame(frameRequetCallback);
+  })
+  .catch(console.error);
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './demo/main.js'),
+    output: {
+        path: path.resolve(__dirname, './docs/demo/'),
+        filename: 'particles.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './demo/index.html'),
+        }),
+    ],
+    mode: 'development',
+};
